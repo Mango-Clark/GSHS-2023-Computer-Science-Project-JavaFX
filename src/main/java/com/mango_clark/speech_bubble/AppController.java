f04902046d71230c981bb7ed17ca69c07b2042e2
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-06-16 10:14:06 +0900

    initial image

--- src/main/java/com/mango_clark/speech_bubble/AppController.java
+++ src/main/java/com/mango_clark/speech_bubble/AppController.java
@@ -1,80 +1,27 @@
 package com.mango_clark.speech_bubble;
 
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 
 public class AppController implements Initializable {
 	@FXML
-	DatePicker datePicker;
-
+	MenuBar menuBar;
 	@FXML
-	TextField round;
-
-	@FXML
-	Button todayBtn;
-
-	@FXML
-	Label todayLabel;
-
-	LocalDate nowDate = LocalDate.now();
-	final LocalDate initalDate = LocalDate.of(2022, 4, 1);
-	LocalDate setDate;
-	long dateDiff;
+	ImageView imageView;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		datePicker.setValue(nowDate);
-		setDate = datePicker.getValue();
-		update();
+		initializeImage();
 	}
 
-	public void todayBtnClick(ActionEvent event) {
-		datePicker.setValue(nowDate);
-		setDate = datePicker.getValue();
-		update();
+	public void initializeImage() {
+		imageView.setImage(new Image(getClass().getResourceAsStream("/files/images/107852415_p4.png")));
 	}
 
-	public void update() {
-		dateDiff = ChronoUnit.DAYS.between(initalDate, setDate);
-		round.setText("" + dateDiff);
-		nowDate = LocalDate.now();
-		todayLabel.setText("회차");
-	}
-
-	public void getDate(ActionEvent event) {
-		setDate = datePicker.getValue();
-		update();
-	}
-
-	public void getRound(ActionEvent event) {
-		if (!isInteger(round.getText())) {
-
-		}
-
-		update();
-	}
-
-	public static boolean isInteger(String s, int radix) {
-		Scanner sc = new Scanner(s.trim());
-		if (!sc.hasNextInt(radix))
-			return false;
-		sc.nextInt(radix);
-		return !sc.hasNext();
-	}
-
-	public static boolean isInteger(String s) {
-		return isInteger(s, 10);
-	}
-
-	void errorMsg(String msg) {
-
-	}
 }
