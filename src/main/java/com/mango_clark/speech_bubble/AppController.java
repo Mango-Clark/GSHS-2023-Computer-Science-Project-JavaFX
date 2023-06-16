d51fb8bfc250e0af831f97f86805c7bcbf9c0af1
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-06-16 14:07:14 +0900

    defined Menu at Controller class

--- src/main/java/com/mango_clark/speech_bubble/AppController.java
+++ src/main/java/com/mango_clark/speech_bubble/AppController.java
@@ -14,14 +14,32 @@ public class AppController implements Initializable {
 	MenuBar menuBar;
 	@FXML
 	ImageView imageView;
+	Menu menu_File, menu_Help;
+	MenuItem menuItem_Open, menuItem_Save;
+	MenuItem menuItem_GitHub;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeImage();
+		initializeMenuBar();
 	}
 
 	public void initializeImage() {
 		imageView.setImage(new Image(getClass().getResourceAsStream("files/images/107852415_p4.png")));
+	}
+
+	private void initializeMenuBar() {
+		menu_File = new Menu("File");
+		menu_Help = new Menu("Help");
+
+		menuItem_Open = new MenuItem("Open");
+		menuItem_Save = new MenuItem("Save");
+		menu_File.getItems().addAll(menuItem_Open, menuItem_Save);
+
+		menuItem_GitHub = new MenuItem("GitHub");
+		menu_Help.getItems().addAll(menuItem_GitHub);
+
+		menuBar.getMenus().addAll(menu_File, menu_Help);
 	}
 
 	public void menuBarRequest(ActionEvent event) {
