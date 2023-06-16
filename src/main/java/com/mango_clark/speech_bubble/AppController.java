c603ef1185cddad8fe7f898211c7b68305785eaf
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-06-16 09:59:12 +0900

    first

---
+++ src/main/java/com/mango_clark/speech_bubble/AppController.java
@@ -0,0 +1,80 @@
+package com.mango_clark.speech_bubble;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class AppController implements Initializable {
+	@FXML
+	DatePicker datePicker;
+
+	@FXML
+	TextField round;
+
+	@FXML
+	Button todayBtn;
+
+	@FXML
+	Label todayLabel;
+
+	LocalDate nowDate = LocalDate.now();
+	final LocalDate initalDate = LocalDate.of(2022, 4, 1);
+	LocalDate setDate;
+	long dateDiff;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		datePicker.setValue(nowDate);
+		setDate = datePicker.getValue();
+		update();
+	}
+
+	public void todayBtnClick(ActionEvent event) {
+		datePicker.setValue(nowDate);
+		setDate = datePicker.getValue();
+		update();
+	}
+
+	public void update() {
+		dateDiff = ChronoUnit.DAYS.between(initalDate, setDate);
+		round.setText("" + dateDiff);
+		nowDate = LocalDate.now();
+		todayLabel.setText("회차");
+	}
+
+	public void getDate(ActionEvent event) {
+		setDate = datePicker.getValue();
+		update();
+	}
+
+	public void getRound(ActionEvent event) {
+		if (!isInteger(round.getText())) {
+
+		}
+
+		update();
+	}
+
+	public static boolean isInteger(String s, int radix) {
+		Scanner sc = new Scanner(s.trim());
+		if (!sc.hasNextInt(radix))
+			return false;
+		sc.nextInt(radix);
+		return !sc.hasNext();
+	}
+
+	public static boolean isInteger(String s) {
+		return isInteger(s, 10);
+	}
+
+	void errorMsg(String msg) {
+
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module com.mango_clark.requestfx {
+	requires java.se;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	requires javafx.graphics;
+}
