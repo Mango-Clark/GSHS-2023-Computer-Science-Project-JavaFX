9eb6ae2429dee32fc740653df39012ae0bd3e31e
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-06-17 13:33:33 +0900

    NO fxml files

--- src/main/java/com/mango_clark/speech_bubble/App.java
+++ src/main/java/com/mango_clark/speech_bubble/App.java
@@ -1,17 +1,31 @@
 package com.mango_clark.speech_bubble;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+	BorderPane rootPane;
+	MenuBar menuBar;
+	ImageView imageView;
+	Image image;
+	Menu menu_File, menu_Help;
+	MenuItem menuItem_Open, menuItem_Save;
+	MenuItem menuItem_GitHub;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
-			Scene scene = new Scene(root);
+			Scene scene = initializeScene();
+			initializeImage();
+			initializeMenuBar(primaryStage);
 
 			primaryStage.setTitle("Gs21079 - Paint Speech Bubble");
 			primaryStage.setScene(scene);
@@ -19,6 +33,44 @@ public class App extends Application {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public Scene initializeScene() {
+		rootPane = new BorderPane();
+		rootPane.setPrefSize(700, 500);
+		menuBar = new MenuBar();
+		rootPane.setTop(menuBar);
+		imageView = new ImageView();
+		imageView.prefHeight(400);
+		imageView.prefWidth(600);
+		rootPane.setLeft(imageView);
+		return new Scene(rootPane);
+	}
+
+	public void initializeImage() {
+		image = new Image(getClass().getResourceAsStream("files/images/107852415_p4.png"), 400, 600, true, true);
+		imageView.setImage(image);
+	}
+
+	private void initializeMenuBar(Stage stage) {
+		menu_File = new Menu("File");
+		menu_Help = new Menu("Help");
+
+		menuItem_Open = new MenuItem("Open");
+		menuItem_Open.setOnAction(e -> {
+			System.out.println("Open");
+		});
+		menuItem_Save = new MenuItem("Save");
+		menuItem_Save.setOnAction(e -> {
+			System.out.println("Save");
+		});
+		menu_File.getItems().addAll(menuItem_Open, menuItem_Save);
+
+		menuItem_GitHub = new MenuItem("GitHub");
+		menuItem_GitHub.setOnAction(e -> AppController.showGithubPopup(stage));
+		menu_Help.getItems().addAll(menuItem_GitHub);
+
+		menuBar.getMenus().addAll(menu_File, menu_Help);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/mango_clark/speech_bubble/AppController.java
+++ src/main/java/com/mango_clark/speech_bubble/AppController.java
@@ -2,48 +2,54 @@ package com.mango_clark.speech_bubble;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.image.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
 public class AppController implements Initializable {
-	@FXML
-	MenuBar menuBar;
-	@FXML
-	ImageView imageView;
-	Menu menu_File, menu_Help;
-	MenuItem menuItem_Open, menuItem_Save;
-	MenuItem menuItem_GitHub;
+
+	static Stage githubPopup;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		initializeImage();
-		initializeMenuBar();
 	}
 
-	public void initializeImage() {
-		imageView.setImage(new Image(getClass().getResourceAsStream("files/images/107852415_p4.png")));
+	public static void showGithubPopup(Stage stage) {
+		Label label = new Label("GitHub page?");
+		label.setFont(new Font("Consolas", 15));
+		label.setLayoutX(75);
+		label.setLayoutY(43);
+		label.setTextAlignment(TextAlignment.CENTER);
+
+		Button open = new Button("Goto GitHub");
+		open.setLayoutX(83);
+		open.setLayoutY(86);
+		open.setOnAction(e -> openGithubPage());
+
+		AnchorPane pane = new AnchorPane(label, open);
+
+		githubPopup = new Stage();
+		githubPopup.setAlwaysOnTop(true);
+		githubPopup.setResizable(false);
+		githubPopup.setHeight(150);
+		githubPopup.setWidth(250);
+		githubPopup.setTitle("GitHub Popup");
+		githubPopup.setScene(new Scene(pane));
+		githubPopup.show();
 	}
 
-	private void initializeMenuBar() {
-		menu_File = new Menu("File");
-		menu_Help = new Menu("Help");
-
-		menuItem_Open = new MenuItem("Open");
-		menuItem_Save = new MenuItem("Save");
-		menu_File.getItems().addAll(menuItem_Open, menuItem_Save);
-
-		menuItem_GitHub = new MenuItem("GitHub");
-		menu_Help.getItems().addAll(menuItem_GitHub);
-
-		menuBar.getMenus().addAll(menu_File, menu_Help);
+	public static void openGithubPage() {
+		try {
+			java.awt.Desktop.getDesktop().browse(
+					java.net.URI.create("https://github.com/Mango-Clark/GSHS-2023-Computer-Science-Project-JavaFX"));
+			githubPopup.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
-	public void menuBarRequest(ActionEvent event) {
-
-	}
-
 }
