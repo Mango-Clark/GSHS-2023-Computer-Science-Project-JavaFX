712869f133e113a8886a3c02626204643f7c9351
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-06-18 22:17:19 +0900

    newer version gson

--- src/main/java/com/mango_clark/speech_bubble/App.java
+++ src/main/java/com/mango_clark/speech_bubble/App.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.nio.file.FileSystems;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
